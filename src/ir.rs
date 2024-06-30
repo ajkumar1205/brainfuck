1269564838aafea97b696dcd9ae16770bf00b11a
Ajay Kumar <ajthakur1205@gmail.com>
2024-06-30 23:31:44 +0530

    Changed IR code

--- src/ir.rs
+++ src/ir.rs
@@ -4,36 +4,64 @@ pub struct Representation {
     pub instructions: Vec<Instruction>,
 }
 // Change it according to standard
-// Added 
+// Added
 impl Representation {
     pub fn new() -> Self {
-        Representation { instructions: vec![] }
-    } 
+        Representation {
+            instructions: vec![],
+        }
+    }
 
-    pub fn parse(&mut self, tokens: Vec<Token>){
-        let mut i = 0;
-        let mut last =  TokenType::Eof;
+    pub fn parse(&mut self, tokens: Vec<Token>) {
+        let mut count = 0;
+        let mut ptr = 0;
+        let mut last = TokenType::Eof;
+
         for token in tokens {
-            let t =  token.token_type();
             
-            if t == TokenType::Print {
-                if i != 0 {
-                    self.instructions.push(Instruction::Write((i-1) as *const u8));
-                } 
-                if i==0 {
-                    
+            match token.token_type() {
+                TokenType::Print => {
+                    self.instructions.push(Instruction::Print(ptr));
                 }
+                TokenType::Read => {
+                    self.instructions.push(Instruction::Read(ptr));
+                }
+                TokenType::LoopStart => {
+                    self.instructions.push(Instruction::LoopStart(ptr));
+                }
+                TokenType::LoopEnd => {
+                    self.instructions.push(Instruction::LoopEnd(ptr));
+                }
+                TokenType::MoveLeft => {
+                    ptr = (ptr + 30000 - 1) % 30000;
+                    count = Representation::get();
+                }
+                TokenType::MoveRight => {
+                    ptr = (ptr + 1) % 30000;
+                }
+                TokenType::Increment => {
+                    count = count + 1;
+                }
+                TokenType::Decrement => {
+                    count = count + 1;
+                }
+                _ => {}
             }
         }
     }
 
+    // GET THE VALUE IN THE PREVIOUS INDEX 
+    // SO THAT WE CAN MOVE LEFT RIGHT WITH 
+    // CORRECT VALUES FOR SUMMATION
+    fn get() -> i32 {
+        0
+    }
 }
 
 pub enum Instruction {
-    Add(i64),
-    Sub(i64),
-    Read(*const u8),
-    Write(*const u8),
-    LoopStart(*const u8),
-    LoopEnd(*const u8),
+    Sum(i32, usize),
+    Read(usize),
+    Print(usize),
+    LoopStart(usize),
+    LoopEnd(usize),
 }
